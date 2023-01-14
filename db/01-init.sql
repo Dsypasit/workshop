39eee04f4b4aebb412292a5b57295f2e16a24b31
Wyt <98400772+wytquant@users.noreply.github.com>
2023-01-14 15:11:48 +0700

    add table transaction

--- db/01-init.sql
+++ db/01-init.sql
@@ -1,5 +1,6 @@
 CREATE SEQUENCE IF NOT EXISTS account_id;
 CREATE SEQUENCE IF NOT EXISTS pocket_id;
+CREATE SEQUENCE IF NOT EXISTS txn_id;
 
 CREATE TABLE "accounts" (
     "id" int4 NOT NULL DEFAULT nextval('account_id'::regclass),
@@ -11,4 +12,14 @@ CREATE TABLE "cloud_pocket" (
     "id" int4 NOT NULL DEFAULT nextval('pocket_id'::regclass),
     "name" TEXT NOT NULL,
     PRIMARY KEY ("id")
+)
+
+CREATE TABLE "txn" (
+    "id" int4 NOT NULL DEFAULT nextval('txn_id'::regclass)
+    "timestamp" TIMESTAMP NOT NULL,
+    "amount" NUMERIC NOT NULL,
+    "note" VARCHAR NOT NULL,
+    "sender" int4 NOT NULL,
+    "receiver" int4 NOTN NULL,
+    PRIMARY KEY ("id") 
 )
