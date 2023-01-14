32f95b103e20299aae084919429992f0a6bba344
ArtCode101 <sanayut12.service@gmail.com>
2023-01-14 11:35:43 +0700

    replace url database

--- config/config.go
+++ config/config.go
@@ -40,7 +40,7 @@ const (
 
 const (
 	dPort         = 1323
-	dDBConnection = "postgresql://postgres:password@localhost:5432/banking?sslmode=disable"
+	dDBConnection = "postgres://clmmgcxu:VMWA4v7CVDqOkPTuZcbpMxOuq676f5sI@tiny.db.elephantsql.com/clmmgcxu"
 )
 
 func (c *cfg) All() Config {
