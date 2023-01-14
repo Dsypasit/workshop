32ed85345020193d6e28773c2559fa27d9d67c91
dsypasit <ong.pasit@gmail.com>
2023-01-14 14:57:58 +0700

    fix: add comma and semicolon

--- db/01-init.sql
+++ db/01-init.sql
@@ -9,9 +9,9 @@ CREATE TABLE "accounts" (
 CREATE TABLE "pockets" (
     "id" int4 NOT NULL DEFAULT nextval('pocket_id'::regclass),
     "balance" float8 NOT NULL DEFAULT 0,
-    "account_id" int4 NOT NULL
-    "name" TEXT NOT NULL
-    "currency" TEXT NOT NULL
-    "category" TEXT NOT NULL
-    PRIMARY KEY ("id")
-)
+    "account_id" int4 NOT NULL,
+    "name" TEXT NOT NULL,
+    "currency" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    PRIMARY KEY ("id"),
+);
