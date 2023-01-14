088344d0b42a70b1c8c972d681387d168be1dda0
wytwit <wittawas.kns@gmail.com>
2023-01-14 11:20:16 +0700

    add pockets table

--- db/01-init.sql
+++ db/01-init.sql
@@ -5,3 +5,13 @@ CREATE TABLE "accounts" (
     "balance" float8 NOT NULL DEFAULT 0,
     PRIMARY KEY ("id")
 );
+
+CREATE TABLE "pockets" (
+    "id" int4 NOT NULL DEFAULT nextval('pocket_id'::regclass),
+    "balance" float8 NOT NULL DEFAULT 0,
+    "account_id" int4 NOT NULL
+    "name" TEXT NOT NULL
+    "currency" TEXT NOT NULL
+    "category" TEXT NOT NULL
+    PRIMARY KEY ("id")
+)
