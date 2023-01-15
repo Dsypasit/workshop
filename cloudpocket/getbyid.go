fc929d6ae09d68146b1da692ca694a0a2c49cfa4
ArtCode101 <sanayut12.service@gmail.com>
2023-01-15 09:56:41 +0700

    add func balance to get pocket by id

--- cloudpocket/getbyid.go
+++ cloudpocket/getbyid.go
@@ -11,21 +11,29 @@ import (
 func (h handler) GetCloudPocketById(c echo.Context) error {
 	id := c.Param("id")
 	pocket := cloudpocket{}
-	stmt, err := h.db.Prepare("SELECT * FROM cloud_pocket WHERE id=$1")
+	stmt, err := h.db.Prepare("SELECT id,name FROM cloud_pocket WHERE id=$1")
 	if err != nil {
 		log.Fatal(err)
 		return c.JSON(http.StatusInternalServerError, err)
 	}
 	intid, err := strconv.Atoi(id)
 	if err != nil {
+		println("1")
 		return c.JSON(http.StatusBadRequest, err)
 	}
 	row := stmt.QueryRow(intid)
 
-	err = row.Scan(&pocket.Id, &pocket.Name, &pocket.Balance)
+	err = row.Scan(&pocket.Id, &pocket.Name)
 	if err != nil {
+		println("2")
 		return c.JSON(http.StatusBadRequest, err)
 	}
+
+	balance, err := h.getBalance(pocket.Id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	pocket.Balance = balance
 	return c.JSON(http.StatusOK, pocket)
 
 }
