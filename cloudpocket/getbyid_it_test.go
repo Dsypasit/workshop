0dd6812054f488faf2926225eb67071bd285b66f
Kittipat <first1801@hotmail.com>
2023-01-15 10:32:27 +0700

    getbyid it test

--- cloudpocket/getbyid_it_test.go
+++ cloudpocket/getbyid_it_test.go
@@ -26,7 +26,6 @@ func TestCreateAccountIT(t *testing.T) {
 	cfgFlag := config.FeatureFlag{}
 
 	h = handler{&sql}
-	reqBody := `{"balance": 999.99}`
 
 	e.GET("/cloud-pockets/:id", h.GetCloundPocketById)
 	e.POST("/cloud-pockets", h.Creat)
@@ -38,13 +37,13 @@ func TestCreateAccountIT(t *testing.T) {
 
 	e.ServeHTTP(rec, req)
 
-	req := httptest.NewRequest(http.MethodPost, "/clound-pockets/1", nil)
+	req := httptest.NewRequest(http.MethodGet, "/clound-pockets/1", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
 	e.ServeHTTP(rec, req)
 
-	expected := `{"id": 1, "balance": 999.99}`
+	expected := `{"id": 1, "name": "test", "balance": 0}`
 	assert.Equal(t, http.StatusCreated, rec.Code)
 	assert.JSONEq(t, expected, rec.Body.String())
 }
